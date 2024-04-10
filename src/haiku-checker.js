62566567f34872ee1145160fffc39a7b6e00c3c4
Aidan Williams <aidanwilliams01@gmail.com>
2024-04-10 14:52:45 -0700

    add passing code

--- src/haiku-checker.js
+++ src/haiku-checker.js
@@ -12,7 +12,16 @@ export function haikuChecker(poem) {
   function syllableChecker(word) {
     // add vowel exceptions
     const wordArray = word.split('');
-    const vowels = wordArray.filter(e => e === 'a' || e === 'e' || e === 'i' || e === 'o' || e === 'u' || e === 'y');
-    return vowels.length;
+    console.log(wordArray);
+    if (wordArray[wordArray.length - 1] === 'e') {
+      const newWordArray = wordArray.slice(0, wordArray.length - 2)
+      console.log(newWordArray);
+      const vowels = newWordArray.filter(e => e === 'a' || e === 'e' || e === 'i' || e === 'o' || e === 'u' || e === 'y');
+      return vowels.length;
+    }
+    else {
+      const vowels = wordArray.filter(e => e === 'a' || e === 'e' || e === 'i' || e === 'o' || e === 'u' || e === 'y');
+      return vowels.length;
+    }
   }
 }
